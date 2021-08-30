29703ce1588f08c6f5829e5cd0f0a39683dd75e7
pashakiz <pashakiz@gmail.com>
2021-08-30 03:55:39 +0300

    fix: fix getWinner

--- src/tic-tac-toe.js
+++ src/tic-tac-toe.js
@@ -6,15 +6,13 @@ const NEXT_PLAYER = {
 }
 class TicTacToe {
 
-  CurrentPlayerSymbol = FIRST_PLAYER
-
-  gameField = [
-    [null, null, null],
-    [null, null, null],
-    [null, null, null]
-  ]
-
     constructor() {
+      this.CurrentPlayerSymbol = FIRST_PLAYER
+      this.gameField = [
+        [null, null, null],
+        [null, null, null],
+        [null, null, null]
+      ]
     }
 
     getCurrentPlayerSymbol() {
@@ -28,27 +26,27 @@ class TicTacToe {
     }
 
     isFinished() {
-      return this.getWinner() || this.isDraw()
+      return !!this.getWinner() || this.noMoreTurns()
     }
 
     getWinner() {
       const winByRow = this.gameField.map(row => row
-                                     .every(v => v === FIRST_PLAYER || v === SECOND_PLAYER))
+                                     .every(v => v === NEXT_PLAYER[this.CurrentPlayerSymbol]))
                                      .findIndex(v => v)
       if (winByRow >= 0) return this.gameField[winByRow][0]
 
       const winByCol = this.gameField[0].map((_, columnIndex) => this.gameField
                                         .map(row => row[columnIndex])
-                                        .every(v => v === FIRST_PLAYER || v === SECOND_PLAYER))
+                                        .every(v => v === NEXT_PLAYER[this.CurrentPlayerSymbol]))
                                         .findIndex(v => v)
       if (winByCol >= 0) return this.gameField[0][winByCol]
 
       const winByCrossA = this.gameField.map((row, i) => row[i])
-                                        .every((v, j, arr) => v === arr[0][0])
+                                        .every((v, j, arr) => v === arr[0])
       if (winByCrossA) return this.gameField[1][1] //main diag
 
       const winByCrossB = this.gameField.map((row, i) => row[2-i])
-                                        .every((v, j, arr) => v === arr[0][2])
+                                        .every((v, j, arr) => v === arr[2])
       if (winByCrossB) return this.gameField[1][1] //not main diag
 
       return null
@@ -63,7 +61,7 @@ class TicTacToe {
     }
 
     getFieldValue(rowIndex, colIndex) {
-      return this.gameField[rowIndex][colIndex] ? this.gameField[rowIndex][colIndex] : null
+      return this.gameField[rowIndex][colIndex]
     }
 }
 
